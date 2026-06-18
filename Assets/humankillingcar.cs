170440f0d6f8cbc8347815e74eece67e3775c87c
semimanboy22 <semvanloo2009@gmail.com>
2026-06-18 14:48:39 +0200

    cars moving corect distance and glitch gone where player can skip checkpoint and not die

--- Assets/humankillingcar.cs
+++ Assets/humankillingcar.cs
@@ -3,10 +3,34 @@ using UnityEngine.SceneManagement;
 using System.Collections.Generic;
 
 [System.Serializable]
-public class RespawnObject
+public class RespawnableObjectCheckpointSettings
+{
+    public int checkpointOrder = 0;
+    public Vector3 respawnPoint = Vector3.zero;
+}
+
+[System.Serializable]
+public class RespawnableObject
 {
     public Transform obj;
-    public Vector3 respawnPosition;
+    [SerializeField]
+    private Vector3 defaultRespawnPoint = Vector3.zero;
+    [SerializeField]
+    private List<RespawnableObjectCheckpointSettings> checkpointRespawnSettings = new List<RespawnableObjectCheckpointSettings>();
+
+    public Vector3 GetDefaultRespawnPoint() => defaultRespawnPoint;
+    public void SetDefaultRespawnPoint(Vector3 point) => defaultRespawnPoint = point;
+    
+    public List<RespawnableObjectCheckpointSettings> GetCheckpointRespawnSettings() => checkpointRespawnSettings;
+}
+
+[System.Serializable]
+public class CheckpointRespawnSettings
+{
+    public int checkpointOrder = 0;
+    public Transform checkpointObject;
+    public Vector3 spawnOffset = new Vector3(1f, 0f, 0f);
+    public Vector3 triggerBoxSize = new Vector3(5f, 5f, 5f);
 }
 
 public class humankillingcar : MonoBehaviour
@@ -20,15 +44,110 @@ public class humankillingcar : MonoBehaviour
     public float killDistance = 2f;
     public Vector3 barrierSize = new Vector3(3f, 3f, 1f);
 
-    [Header("Respawn Settings")]
-    public List<RespawnObject> objectsToRespawn = new List<RespawnObject>();
+    [Header("Player Respawn")]
+    public Vector3 playerOriginalSpawnPoint = Vector3.zero;
+
+    [Header("Respawnable Objects")]
+    public List<RespawnableObject> respawnableObjects = new List<RespawnableObject>();
+
+    [Header("Checkpoint Respawn Settings")]
+    public List<CheckpointRespawnSettings> checkpointSettings = new List<CheckpointRespawnSettings>();
 
     private Vector3 startPosition;
     private float distanceTraveled = 0f;
+    
+    // Checkpoint system
+    private int lastCheckpointOrderReached = -1;
+
+    public void SetPlayerSpawnPoint(Vector3 newSpawnPoint)
+    {
+        playerOriginalSpawnPoint = newSpawnPoint;
+    }
+
+    public void SetRespawnableObjectDefaultSpawn(RespawnableObject respawnObj, Vector3 spawnPoint)
+    {
+        respawnObj.SetDefaultRespawnPoint(spawnPoint);
+    }
+
+    public void SetRespawnableObjectCheckpointSpawn(RespawnableObject respawnObj, int checkpointOrder, Vector3 spawnPoint)
+    {
+        var checkpointSettings = respawnObj.GetCheckpointRespawnSettings();
+        
+        // Find if checkpoint setting already exists
+        for (int i = 0; i < checkpointSettings.Count; i++)
+        {
+            if (checkpointSettings[i].checkpointOrder == checkpointOrder)
+            {
+                checkpointSettings[i].respawnPoint = spawnPoint;
+                return;
+            }
+        }
+        
+        // Create new checkpoint setting if it doesn't exist
+        RespawnableObjectCheckpointSettings newSetting = new RespawnableObjectCheckpointSettings
+        {
+            checkpointOrder = checkpointOrder,
+            respawnPoint = spawnPoint
+        };
+        checkpointSettings.Add(newSetting);
+    }
 
     void Start()
     {
         startPosition = transform.position;
+    }
+
+    public void RegisterCheckpoint(int checkpointOrder, Vector3 checkpointPosition)
+    {
+        // Update last checkpoint order if this is newer
+        if (checkpointOrder > lastCheckpointOrderReached)
+        {
+            lastCheckpointOrderReached = checkpointOrder;
+        }
+    }
+
+    private Vector3 GetRespawnPositionForCheckpoint()
+    {
+        // If no checkpoint reached, respawn at player's original spawn point
+        if (lastCheckpointOrderReached == -1)
+        {
+            return playerOriginalSpawnPoint;
+        }
+
+        // Find the checkpoint with the highest order reached
+        for (int i = 0; i < checkpointSettings.Count; i++)
+        {
+            if (checkpointSettings[i].checkpointOrder == lastCheckpointOrderReached && checkpointSettings[i].checkpointObject != null)
+            {
+                // Return the spawn offset position directly
+                return checkpointSettings[i].checkpointObject.position + checkpointSettings[i].spawnOffset;
+            }
+        }
+
+        // Fallback to start position if checkpoint not found
+        return startPosition;
+    }
+
+    private Vector3 GetRespawnPositionForRespawnableObject(RespawnableObject respawnableObj)
+    {
+        // If no checkpoint reached, use default respawn point
+        if (lastCheckpointOrderReached == -1)
+        {
+            return respawnableObj.GetDefaultRespawnPoint();
+        }
+
+        // Find the checkpoint-specific respawn point for this object
+        var checkpointSettings = respawnableObj.GetCheckpointRespawnSettings();
+        for (int i = 0; i < checkpointSettings.Count; i++)
+        {
+            if (checkpointSettings[i].checkpointOrder == lastCheckpointOrderReached)
+            {
+                return checkpointSettings[i].respawnPoint;
+            }
+        }
+
+        // If no checkpoint-specific respawn point found, use default
+        return respawnableObj.GetDefaultRespawnPoint();
     }
 
     void Update()
@@ -62,12 +181,14 @@ public class humankillingcar : MonoBehaviour
             }
             else
             {
-                // Check if hit object is in the respawn list
-                for (int i = 0; i < objectsToRespawn.Count; i++)
+                // Check if hit object is in the respawnable list
+                for (int i = 0; i < respawnableObjects.Count; i++)
                 {
-                    if (hit.transform == objectsToRespawn[i].obj)
+                    if (hit.transform == respawnableObjects[i].obj)
                     {
-                        RespawnObjectAtPosition(hit.transform, objectsToRespawn[i].respawnPosition);
+                        // Respawn object at its configured respawn point based on checkpoint
+                        Vector3 respawnPos = GetRespawnPositionForRespawnableObject(respawnableObjects[i]);
+                        RespawnObjectAtPosition(hit.transform, respawnPos);
                         break;
                     }
                 }
@@ -90,50 +211,89 @@ public class humankillingcar : MonoBehaviour
 
     void RespawnObject(Transform obj)
     {
-        // Find spawn point object in scene
-        GameObject spawn = GameObject.FindWithTag("SpawnPoint");
+        // Respawn player at the last checkpoint reached, or start position if none reached
+        Vector3 respawnPos = GetRespawnPositionForCheckpoint();
+        
+        CharacterController cc = obj.GetComponent<CharacterController>();
 
-        if (spawn != null)
-        {
-            CharacterController cc = obj.GetComponent<CharacterController>();
+        if (cc != null)
+            cc.enabled = false;
 
-            // Disable CharacterController temporarily if needed
-            if (cc != null)
-                cc.enabled = false;
+        obj.position = respawnPos;
 
-            obj.position = spawn.transform.position;
-
-            if (cc != null)
-                cc.enabled = true;
-        }
+        if (cc != null)
+            cc.enabled = true;
     }
 
     // Draw invisible barrier in editor
     void OnDrawGizmos()
     {
-        Gizmos.color = Color.red;
-
-        Vector3 barrierPosition = transform.position + moveDirection.normalized * killDistance;
-
-        Gizmos.matrix = Matrix4x4.TRS(
-            barrierPosition,
-            transform.rotation,
-            Vector3.one
-        );
-
-        Gizmos.DrawWireCube(Vector3.zero, barrierSize);
-
-        // Reset matrix before drawing respawn spheres
+        // Reset matrix
         Gizmos.matrix = Matrix4x4.identity;
 
-        // Draw respawn positions as blue spheres
-        Gizmos.color = Color.blue;
-        foreach (RespawnObject respawnObj in objectsToRespawn)
+        // Draw all checkpoint trigger boxes and spawn offsets
+        foreach (CheckpointRespawnSettings setting in checkpointSettings)
         {
-            if (respawnObj.obj != null)
+            if (setting.checkpointObject != null)
             {
-                Gizmos.DrawSphere(respawnObj.respawnPosition, 0.5f);
+                Vector3 checkpointPos = setting.checkpointObject.position;
+                
+                // Draw trigger box as blue outline
+                Gizmos.color = Color.blue;
+                Gizmos.matrix = Matrix4x4.TRS(checkpointPos, Quaternion.identity, Vector3.one);
+                Gizmos.DrawWireCube(Vector3.zero, setting.triggerBoxSize);
+                Gizmos.matrix = Matrix4x4.identity;
+                
+                // Draw checkpoint position as white sphere
+                Gizmos.color = Color.white;
+                Gizmos.DrawSphere(checkpointPos, 0.5f);
+                
+                // Draw spawn offset position as green sphere (where player will respawn)
+                Vector3 spawnPos = checkpointPos + setting.spawnOffset;
+                Gizmos.color = Color.green;
+                Gizmos.DrawSphere(spawnPos, 0.3f);
+                
+                // Draw line from checkpoint to spawn position
+                Gizmos.color = Color.yellow;
+                Gizmos.DrawLine(checkpointPos, spawnPos);
             }
+        }
+
+        // Draw respawnable objects' positions and respawn points
+        Gizmos.color = Color.cyan;
+        foreach (RespawnableObject respObj in respawnableObjects)
+        {
+            if (respObj.obj != null)
+            {
+                // Draw current object position
+                Gizmos.DrawSphere(respObj.obj.position, 0.3f);
+                
+                // Draw default respawn point
+                Gizmos.color = Color.magenta;
+                Gizmos.DrawSphere(respObj.GetDefaultRespawnPoint(), 0.25f);
+                
+                // Draw line from object to default respawn point
+                Gizmos.color = new Color(1, 0, 1, 0.5f);
+                Gizmos.DrawLine(respObj.obj.position, respObj.GetDefaultRespawnPoint());
+                
+                // Draw checkpoint-specific respawn points
+                foreach (RespawnableObjectCheckpointSettings checkpointSetting in respObj.GetCheckpointRespawnSettings())
+                {
+                    Gizmos.color = new Color(0, 1, 1, 0.7f);
+                    Gizmos.DrawSphere(checkpointSetting.respawnPoint, 0.2f);
+                }
+            }
+        }
+
+        // Draw current player respawn position as large blue sphere preview
+        if (playerOriginalSpawnPoint != Vector3.zero)
+        {
+            Gizmos.color = Color.blue;
+            Gizmos.DrawSphere(playerOriginalSpawnPoint, 0.5f);
+            
+            // Draw a semi-transparent outer sphere for better visibility
+            Gizmos.color = new Color(0, 0, 1, 0.3f);
+            Gizmos.DrawWireSphere(playerOriginalSpawnPoint, 0.7f);
         }
     }
 }
